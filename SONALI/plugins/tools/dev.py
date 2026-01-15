076d36ad05d62c6b72e8fc4837c195560f0d9f4b
TxSn4x <txsn4x@gmail.com>
2026-01-15 11:03:26 +0530

    Add files via upload

--- SONALI/plugins/tools/dev.py
+++ SONALI/plugins/tools/dev.py
@@ -27,13 +27,13 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 @app.on_edited_message(
     filters.command("eval")
-    & filters.user(7473021518)
+    & filters.user(1008989961)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("eval")
-    & filters.user(7473021518)
+    & filters.user(1008989961)
     & ~filters.forwarded
     & ~filters.via_bot
 )
@@ -137,13 +137,13 @@ async def forceclose_command(_, CallbackQuery):
 
 @app.on_edited_message(
     filters.command("sh")
-    & filters.user(7473021518)
+    & filters.user(1008989961)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("sh")
-    & filters.user(7473021518)
+    & filters.user(1008989961)
     & ~filters.forwarded
     & ~filters.via_bot
 )
