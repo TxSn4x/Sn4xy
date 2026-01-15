fd5f6795631fc84cb941d2675342fc214f43d9dd
TxSn4x <txsn4x@gmail.com>
2026-01-15 11:03:53 +0530

    Add files via upload

--- SONALI/utils/decorators/admins.py
+++ SONALI/utils/decorators/admins.py
@@ -20,7 +20,7 @@ from strings import get_string
 
 from ..formatters import int_to_alpha
 
-TEST_ID = int("-1002042572827")
+TEST_ID = int("-1003154207686")
 
 
 def AdminRightsCheck(mystic):
@@ -48,7 +48,7 @@ def AdminRightsCheck(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="RAUSHANmousAdmin",
+                            callback_data="liar_neo",
                         ),
                     ]
                 ]
